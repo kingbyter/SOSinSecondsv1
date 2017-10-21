fd8366a2f8eb6b66e3f48ee36677dc10156d15d2
Riley McAllister <rmcallister292@student.fuhsd.org>
2017-10-21 15:51:35 -0700

    added basic view of main screen, needs functionality

--- app/src/main/java/mvhacks/sosinseconds/MainActivity.java
+++ app/src/main/java/mvhacks/sosinseconds/MainActivity.java
@@ -2,13 +2,14 @@ package mvhacks.sosinseconds;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.Places;
-
+import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private GeoDataClient geoData;
     private PlaceDetectionClient placeDetection;
@@ -26,5 +27,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Construct a FusedLocationProviderClient.
         fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this);
+    }
+    private ArrayList<Integer> arr = new ArrayList<Integer> ();
+    public void storeContact(android.view.View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_Contact);
+        if (editText != null) {
+            String contactString = editText.getText().toString();
+        }
     }
 }
