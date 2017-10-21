7381010a756e109f0dc4ce473a7f698484d818e7
Raj Gorthi <rajgorthi@gmail.com>
2017-10-21 16:55:26 -0700

    We need to hurry up

--- app/src/main/java/mvhacks/sosinseconds/MainActivity.java
+++ app/src/main/java/mvhacks/sosinseconds/MainActivity.java
@@ -46,10 +46,20 @@ public class MainActivity extends AppCompatActivity {
         fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this);
 
     }
+
+
+
+
     public void sendLocation(){
         ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                 MY_PERMISSIONS_REQUEST_USE_LOCATION);
     }
+
+
+
+
+
+
 
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
