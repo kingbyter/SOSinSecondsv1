86456b3fb36f604f67c67d8b891fa2a8532157f7
Raj Gorthi <rajgorthi@gmail.com>
2017-10-21 11:41:05 -0700

    Initial commit

---
+++ app/src/main/java/mvhacks/sosinseconds/MainActivity.java
@@ -0,0 +1,13 @@
+package mvhacks.sosinseconds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
