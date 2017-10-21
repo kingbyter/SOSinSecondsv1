fed93c19b79bb3c245a21f23ca91473aeaee9909
Raj Gorthi <rajgorthi@gmail.com>
2017-10-21 16:08:59 -0700

    Added asking for permissions for location use

--- app/src/main/java/mvhacks/sosinseconds/MainActivity.java
+++ app/src/main/java/mvhacks/sosinseconds/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private PlaceDetectionClient placeDetection;
     private FusedLocationProviderClient fusedLocationProvider;
 
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
